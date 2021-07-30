e79dc9caa491e956bab02063f2ca9c80c97f84d9
Andrew Bolaji <andrewbolaji@gmail.com>
2021-07-30 16:27:06 -0500

    pubspec fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tik_tok_ui/pages/root_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: RootApp(),
   ));
 }
-
